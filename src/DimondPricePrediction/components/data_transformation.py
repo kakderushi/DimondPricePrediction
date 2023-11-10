10881bbef0181eb7f1adf15ce9cbfa8b8c736dd0
Rushikeh kakde <kakderushikesh81@gmail.com>
2023-11-10 17:33:08 +0530

    last commit

--- src/DimondPricePrediction/components/data_transformation.py
+++ src/DimondPricePrediction/components/data_transformation.py
@@ -91,7 +91,7 @@ class DataTransformation:
             preprocessing_obj = self.get_data_transformation()
             
             target_column_name = 'price'
-            drop_columns = [target_column_name,'id']
+            drop_columns = [target_column_name]
             
             input_feature_train_df = train_df.drop(columns=drop_columns,axis=1)
             target_feature_train_df=train_df[target_column_name]
